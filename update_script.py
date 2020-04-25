56409990b3e2d50ba00d9668ef84faca58b8db63
rahulrajpl <lukrahul@gmail.com>
2020-04-25 23:35:21 +0530

    udated 25Apr

---
+++ update_script.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# ------
+# Credits: Rahul Raj (c) MIT License 2020
+# Web: https://randomwalk.in
+# ------
+
+import os
+from datetime import datetime
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/coder-amey/COVID-19-India_Data/master/time-series/India_regional_aggregated.csv')
+
+# For the bubble chart
+# --------------------
+def format_date(date):
+    l = date.split('-')
+    l = reversed(l)
+    return ''.join(l)
+
+df['Date']=df.Date.apply(format_date)
+cols = ['Region', 'Date', 'Confirmed', 'Recovered/Migrated', 'Deceased']
+df_ind=df.reindex(columns=cols)
+df_ind.drop(index=df_ind[df_ind['Region']=='National Total'].index, inplace=True)
+
+# backup the old data and update new datasource for the bubble chart
+dtg = str(datetime.now())
+dtg = dtg.split()[0]
+cmd = f'mv ./bubble/data/complete_modified_date.csv ./bubble/data/complete_modified_date_{dtg}.csv'
+os.system(cmd)
+df_ind.to_csv('./bubble/data/complete_modified_date.csv', index=False)
+print('Bubble chart data updated')
+
+# For the line chart
+# --------------------
+def format_date(date):
+    l = date.split('-')
+    l = reversed(l)
+    return ''.join(l)
+
+df['Date']=df.Date.apply(format_date)
+cols = ['Region', 'Date', 'Confirmed', 'Recovered/Migrated', 'Deceased']
+df_ind=df.reindex(columns=cols)
+# df_ind.drop(index=df_ind[df_ind['Region']=='National Total'].index, inplace=True)
+
+# backup the old data and update new datasource for the line chart
+cmd = f'mv ./line/data/complete_modified_date.csv ./line/data/complete_modified_date_{dtg}.csv'
+os.system(cmd)
+df_ind.to_csv('./line/data/complete_modified_date.csv', index=False)
+print('Line chart data updated')
+
+print('Completed successfully')
+
